82d5769785b86f3bfe401658191709e2640ad9ca
Glitch (glitch-hello-website) <none>
2023-09-27 11:14:21 +0000

    ☂️🚶 Checkpoint ./scripts/web-functions.js:55379005/582 ./database/database.js:55379005/344 ./index.html:55379005/66

--- scripts/web-functions.js
+++ scripts/web-functions.js
@@ -1,9 +1,9 @@
-import database from "../database/database.js";
-import listToFilter from "../database/database.js";
-import dataProcessing from "../database/database.js";
+import data from "../database/database.js";
 
-// var testFilter = dataProcessing.getDataAdvance(10, 10);
-// console.log(testFilter);
-console.log(database);
-console.log(listToFilter);
-console.log(dataProcessing);
+function log() {
+  console.log(data.database);
+  console.log(data.listToFilter);
+  console.log(data.dataProcessing);  
+}
+
+$('')
