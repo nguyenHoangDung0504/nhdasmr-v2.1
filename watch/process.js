8f247986fd9564f247f856f06f3be948926267b0
Glitch (glitch-hello-website) <none>
2023-10-13 16:14:33 +0000

    🎀🍵 Checkpoint ./watch/process.js:55379005/921 ./watch/style.css:55379005/60

--- watch/process.js
+++ watch/process.js
@@ -11,8 +11,28 @@ function buildIframeAndContent() {
   let tag = document.querySelector('#track_list_tag');
   let data = databaseTypeObject[index];
   name.innerHTML = '<b>'+data.rjCode+'</b> - '+data.engName+'<br>(Original Name: '+data.japName+')';
-  let cvString = '';
-  for() {
-    
+  
+  let cvString = (data.cvs.length>1)?'CVs: ':'CV: ';
+  for(let i=0; i<data.cvs.length; i++) {
+    if(i<data.cvs.length-1) {
+      cvString += '<a class="cv" href="https://nhdasmr-v4.glitch.me/?cv='+data.cvs[i]+'">'+data.cvs[i]+'</a>, ';
+    } else {
+      cvString += '<a class="cv" href="https://nhdasmr-v4.glitch.me/?cv='+data.cvs[i]+'">'+data.cvs[i]+'</a>';
+    }
   }
+  cv.innerHTML = cvString;
+  
+  let tagString = 'Tags: ';
+  for(let i=0; i<data.tags.length; i++) {
+    if(i<data.tags.length-1) {
+      tagString += '<a class="hover" href="https://nhdasmr-v4.glitch.me/?tag='+data.tags[i]+'">'+data.tags[i]+'</a>, ';
+    } else {
+      tagString += '<a class="hover" href="https://nhdasmr-v4.glitch.me/?tag='+data.tags[i]+'">'+data.tags[i]+'</a>';
+    }
+  }
+  tag.innerHTML = tagString; 
+}
+
+function buildListRandom() {
+  
 }
