604de66df50de68566ba21660b00a1669ab0502b
Glitch (glitch-hello-website) <none>
2023-10-15 04:41:30 +0000

    ✨🛍 Checkpoint ./upload/process.js:55379005/3621

--- upload/process.js
+++ upload/process.js
@@ -1,3 +1,21 @@
+function removeCommasAndTrim(str) {
+  let trimmedStr = str.replace(/^,+\s*|\s*,+$/g, '');
+  return trimmedStr;
+}
+
+
 function handleForm() {
-  document.querySelector('body').innerHTML += document.querySelector('#code').value;
+  let code = document.querySelector('#code').value;
+  let rjcode = document.querySelector('#rjcode').value;
+  let cvs = document.querySelector('#cvs').value;
+  let japname = document.querySelector('#japname').value;
+  let engname = document.querySelector('#engname').value;
+  let thumbnail = document.querySelector('#thumbnail').value;
+  let tags = document.querySelector('#tags').value;
+  let images = document.querySelector('#images').value;
+  let tracks = document.querySelector('#tracks').value;
+  
+  let str = `pushData(${code}, "${rjcode}", "${cvs}", "${japname}", "${engname}", "${thumbnail}", "${tags}", "${images}", "${tracks}");`;
+  
+  document.querySelector('body').innerHTML += str;
 }
