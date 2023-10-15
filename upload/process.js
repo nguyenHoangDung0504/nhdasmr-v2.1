3e0b4bcb4d0107f31010e76e149b47fd1bfddcd3
Glitch (glitch-hello-website) <none>
2023-10-15 06:28:55 +0000

    🎐🎢 Checkpoint ./database/database.js:55379005/20 ./watch/process.js:55379005/234 ./upload/process.js:55379005/366

--- upload/process.js
+++ upload/process.js
@@ -5,8 +5,11 @@ function removeCommasAndTrim(str) {
 
 
 function handleForm() {
+  let error = '';
   let code = document.querySelector('#code').value;
+  if(database.code.indexOf(code)!=-1) {error+="trùng code<br>"}
   let rjcode = document.querySelector('#rjcode').value;
+  if(database.rjCode.indexOf(rjcode)!=-1) {error+="trùng rjcode<br>"}
   let cvs = removeCommasAndTrim(document.querySelector('#cvs').value);
   let japname = document.querySelector('#japname').value;
   let engname = document.querySelector('#engname').value;
@@ -14,6 +17,11 @@ function handleForm() {
   let tags =  removeCommasAndTrim(document.querySelector('#tags').value);
   let images = removeCommasAndTrim(document.querySelector('#images').value);
   let tracks = removeCommasAndTrim(document.querySelector('#tracks').value);
+  
+  if(error != '') {
+    document.querySelector('#result').innerHTML += '<span style="color: red;">'+error+'</span>';
+    return;
+  }
   
   let str = `pushData(${code}, "${rjcode}", "${cvs}", "${japname}", "${engname}", "${thumbnail}", "${tags}", "${images}", "${tracks}");`;
   
