25377e52ec6ea7b13e7bd731fa58bf03b960f293
Glitch (glitch-hello-website) <none>
2023-10-15 04:52:31 +0000

    👨🌍 Checkpoint ./upload/process.js:55379005/1272 ./upload/index.html:55379005/527

--- upload/process.js
+++ upload/process.js
@@ -1,5 +1,5 @@
 function removeCommasAndTrim(str) {
-  let trimmedStr = str.replace(/^,+\s*|\s*,+$/g, '');
+  let trimmedStr = str.replace(/^[,\s]+|[,\s]+$/g, '').trim();
   return trimmedStr;
 }
 
@@ -7,15 +7,27 @@ function removeCommasAndTrim(str) {
 function handleForm() {
   let code = document.querySelector('#code').value;
   let rjcode = document.querySelector('#rjcode').value;
-  let cvs = document.querySelector('#cvs').value;
+  let cvs = removeCommasAndTrim(document.querySelector('#cvs').value);
   let japname = document.querySelector('#japname').value;
   let engname = document.querySelector('#engname').value;
   let thumbnail = document.querySelector('#thumbnail').value;
-  let tags = document.querySelector('#tags').value;
-  let images = document.querySelector('#images').value;
-  let tracks = document.querySelector('#tracks').value;
+  let tags =  removeCommasAndTrim(document.querySelector('#tags').value);
+  let images = removeCommasAndTrim(document.querySelector('#images').value);
+  let tracks = removeCommasAndTrim(document.querySelector('#tracks').value);
   
   let str = `pushData(${code}, "${rjcode}", "${cvs}", "${japname}", "${engname}", "${thumbnail}", "${tags}", "${images}", "${tracks}");`;
   
-  document.querySelector('body').innerHTML += str;
+  document.querySelector('#result').innerHTML += str+'<br>';
+}
+
+function reserForm() {
+  document.querySelector('#code').value = '';
+  document.querySelector('#rjcode').value = '';
+  document.querySelector('#cvs').value = '';
+  document.querySelector('#japname').value = '';
+  document.querySelector('#engname').value = '';
+  document.querySelector('#thumbnail').value = '';
+  document.querySelector('#tags').value = '';
+  document.querySelector('#images').value = '';
+  document.querySelector('#tracks').value = '';
 }
