287f1f3d9adfd9db409b3355ad0dc85fe3ca745b
Glitch (glitch-hello-website) <none>
2023-10-28 09:57:50 +0000

    🏰🌋 Checkpoint ./txt/process.js:55379005/1616 ./txt/index.html:55379005/231 ./txt/file.php:55379005/40 ./txt/queue.txt:55379005/16

--- txt/process.js
+++ txt/process.js
@@ -1,12 +1,25 @@
 var request = new XMLHttpRequest();
+var innerData = '';
 request.open('GET', 'txt/queue.txt', true);
 request.onreadystatechange = function() {
   if (request.readyState === 4 && request.status === 200) {
     var lines = request.responseText.split('\n'); // Tách nội dung thành các dòng
 
     lines.forEach(function(line) {
-      document.querySelector()
+      let dataInLine = line.split(',');
+      if(dataInLine.length>=2) {
+        dataInLine.forEach(function(col) {
+          let code = col.split('*')[0];
+          let mess = col.split('*')[1];
+          innerData += `
+            <a>`+col+` `+mess?mess:''+`</a>
+          `;        
+        });        
+      } else {
+        
+      }
     });
   }
+  document.querySelector('body').innerHTML = innerData;
 };
 request.send();
