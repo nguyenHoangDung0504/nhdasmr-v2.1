8cf8207089b2ea125ce999c749f9bc45997502ca
Glitch (glitch-hello-website) <none>
2023-10-31 11:12:52 +0000

    🐏🛣 Checkpoint ./test-data/test-data.js:55379005/557

--- test-data/test-data.js
+++ test-data/test-data.js
@@ -329,8 +329,13 @@ function leakLinkImgAndAud(code){
   let linkBox = document.querySelectorAll('.css-vurnku');
   for(let i=0; i<linkBox.length; i++) {
     if(linkBox[i].innerHTML.indexOf(code) != -1) {
-      linkBox[i].querySelector('div[type="button"]').click();
-      setTimeout(function(){console.log(document.querySelector('button.css-2wa2b5'))/*document.querySelector('.css-2wa2b5').click()*/}, 200);
+      setTimeout(function() {
+        linkBox[i].querySelector('div[type="button"]').click();
+        setTimeout(function() {
+          document.querySelector('.css-2wa2b5').click();
+          document.querySelector('.css-10jd2z1').click();
+        }, 1000);
+      }, 3000);
     }
   }
 }
