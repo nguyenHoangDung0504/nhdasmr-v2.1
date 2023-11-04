3fac739b1aaab3e58f8f68e65ffcb0e73f51738a
Glitch (glitch-hello-website) <none>
2023-11-04 15:31:52 +0000

    🐡⏰ Checkpoint ./download/index.html:55379005/4 ./download/process.js:55379005/3221

--- download/process.js
+++ download/process.js
@@ -21,6 +21,17 @@ function extractNumberFromLink(link) {
     return number;
 }
 
+function isVideoLink(link) {
+  return fetch(link, { method: 'HEAD' })
+    .then(response => {
+      const contentType = response.headers.get('content-type');
+      return contentType.includes('video');
+    })
+    .catch(error => {
+      console.error('Lỗi khi gửi yêu cầu HEAD:', error);
+      return false;
+    });
+}
 function downloadZip() {
     var zip = new JSZip();
     const filename = `${data.code} - ${data.rjCode}`;
@@ -37,37 +48,71 @@ function downloadZip() {
                                 Files are being compressed for download. This will take a while.<br>
                                 (Các file đang được nén lại để tải xuống. Việc này sẽ mất một lúc.)`;
 
-    Promise.all(
+//     Promise.all(
+//         imageLinks.map(function(link) {
+//             return fetch(link).then(function(res) {
+//                 return res.blob();
+//             });
+//         })
+//     )
+//     .then(function(imageBlobs) {
+//         imageBlobs.forEach(function(blob, index) {
+//             const imageNumber = extractNumberFromLink(imageLinks[index]);
+//             folder.file(`${imageNumber}.jpg`, blob, { binary: true });
+//         });
+
+//         Promise.all(
+//             audioLinks.map(function(link) {
+//                 return fetch(link).then(function(res) {
+//                     return res.blob();
+//                 });
+//             })
+//         )
+//         .then(function(audioBlobs) {
+//             audioBlobs.forEach(function(blob, index) {
+//                 const audioNumber = extractNumberFromLink(audioLinks[index]);
+//                 folder.file(`${audioNumber}.mp3`, blob, { binary: true });
+//             });
+
+//             zip.generateAsync({ type: 'blob' })
+//                 .then(function(content) {
+//                     saveAs(content, filename);
+//                     statusElement.innerHTML = 'Done!';
+//                 });
+//         });
+//     });
+      Promise.all(
         imageLinks.map(function(link) {
-            return fetch(link).then(function(res) {
-                return res.blob();
-            });
+          return fetch(link).then(function(res) {
+            return res.blob();
+          });
         })
-    )
-    .then(function(imageBlobs) {
-        imageBlobs.forEach(function(blob, index) {
+      )
+        .then(function(imageBlobs) {
+          imageBlobs.forEach(function(blob, index) {
             const imageNumber = extractNumberFromLink(imageLinks[index]);
             folder.file(`${imageNumber}.jpg`, blob, { binary: true });
-        });
+          });
 
-        Promise.all(
+          Promise.all(
             audioLinks.map(function(link) {
-                return fetch(link).then(function(res) {
-                    return res.blob();
-                });
+              return fetch(link).then(function(res) {
+                return res.blob();
+              });
             })
-        )
-        .then(function(audioBlobs) {
-            audioBlobs.forEach(function(blob, index) {
+          )
+            .then(function(audioBlobs) {
+              audioBlobs.forEach(function(blob, index) {
                 const audioNumber = extractNumberFromLink(audioLinks[index]);
-                folder.file(`${audioNumber}.mp3`, blob, { binary: true });
-            });
+                const audioExtension = isVideoLink(audioLinks[index]) ? '.mp4' : '.mp3';
+                folder.file(`${audioNumber}${audioExtension}`, blob, { binary: true });
+              });
 
-            zip.generateAsync({ type: 'blob' })
+              zip.generateAsync({ type: 'blob' })
                 .then(function(content) {
-                    saveAs(content, filename);
-                    statusElement.innerHTML = 'Done!';
+                  saveAs(content, filename);
+                  statusElement.innerHTML = 'Done!';
                 });
+            });
         });
-    });
 }
